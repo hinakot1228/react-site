0797320321c0758f9a5e0b19a5ab2f64c006fc21
Hinako-Tanaka <youngbird1228@gmail.com>
2021-04-17 16:47:26 +0900

    set

---
+++ src/Blog.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import Article from "./Article";
+import * as FooBar from './components/FooBar';
+import Hoge from './components/Hoge';
+
+const Blog = () => {
+    // componentDidMount(){
+    //     document.getElementById('counter').addEventListener('click', this.countUp)
+    // }
+
+    // componentDidUpdate() {
+    //     if (this.state.count >= 10) {
+    //         this.setState({count:0})
+    //     }
+    // }
+    // componentWillUnmount() {
+    //     document.getElementById('counter').removeEventListener('click', this.countUp)
+    // }
+
+    // countUp = () => {
+    //     this.setState({count:this.state.count +1})
+    // }
+    return (
+        <>
+            <Article 
+                title={"React"} 
+                // count={this.state.count}    
+            />
+            <FooBar.Foo />
+            <FooBar.Bar />
+            <Hoge />
+        </>
+    )
+}
+export default Blog;
---
+++ src/components/FooBar.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+export function Foo() {
+    return(<h2>Fooooo</h2>)
+}
+export const Bar = () => {
+    return(<h2>Baaaar</h2>)
+};
